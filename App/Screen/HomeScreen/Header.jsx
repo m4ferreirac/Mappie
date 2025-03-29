828314f5b2d9f5e17836c2241cf8153ce6cc19f6
m4ferreirac <147309579+m4ferreirac@users.noreply.github.com>
2025-03-29 03:03:03 +0000

    Update Header.jsx

--- App/Screen/HomeScreen/Header.jsx
+++ App/Screen/HomeScreen/Header.jsx
@@ -1,30 +1,13 @@
 import { View, StyleSheet, Image } from "react-native";
 import React from "react";
-import { useUser } from "@clerk/clerk-expo";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function Header() {
-  const { user } = useUser();
-
   return (
     <View style={styles.container}>
       <Image
-        source={{ uri: user?.imageUrl }}
-        style={{
-          width: 45,
-          height: 45,
-          borderRadius: 99,
-        }}
-      />
-      <Image
         source={require("../../../assets/images/Logo.png")}
-        style={{
-          width: 200,
-          height: 45,
-          objectFit: "contain",
-        }}
+        style={styles.logo}
       />
-      <FontAwesome name="filter" size={26} color="black" />
     </View>
   );
 }
@@ -33,8 +16,13 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 50,
     display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
     alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  logo: {
+    width: 200,
+    height: 45,
+    objectFit: "contain",
   },
 });
